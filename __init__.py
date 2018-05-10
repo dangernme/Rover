3bdc5bd5b05bc64cd7cfa45e3a58ae703c670ccb
DangerNME <netking1984@gmx.de>
2018-05-10 18:30:00 +0000

    Keine ahnung

--- __init__.py
+++ __init__.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template ,request
 import sys
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+from importlib.machinery import SourceFileLoader
+
+tmp = SourceFileLoader("module.name", "/var/www/RoverApp/RoverApp/python/buzzer.py").load_module()
+buz = tmp.Buzzer()
 
 app = Flask(__name__)  
 
 @app.route('/') 
 @app.route('/controls')
-def controls():     
-    return render_template("controls.html")
+def controls():   
+    app.logger.warning("bla bla")
+    return render_template("controls.html")#, dir=buz)
 
 @app.route('/configuration') 
 def configuration():     
@@ -17,4 +25,7 @@ def debug():
 	return render_template("debug.html")
 
 if __name__ == "__main__":
+    handler = RotatingFileHandler('rover.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
     app.run()	
